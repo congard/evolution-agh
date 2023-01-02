69a0d01e10bd4db12ef8fdbf50d99a53424c0de3
Daniel <dbcongard@gmail.com>
2023-01-02 23:19:22 +0100

    Cleanup build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,6 @@ javafx {
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 
     // https://mvnrepository.com/artifact/org.json/json
